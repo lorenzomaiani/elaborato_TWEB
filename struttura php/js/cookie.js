2e73e558272ecf57400850a5295a354ff2f4d16f
LorenzoMaiani0 <73821423+lorenzomaiani@users.noreply.github.com>
2022-01-30 12:48:26 +0100

    delete files in struttura php
    delete files in struttura php

--- struttura php/js/cookie.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("input[name=cookie]").click(function(e){
-        e.preventDefault();
-        console.log("OK clicked");
-        $("footer").css("display","none");
-    });
-});
