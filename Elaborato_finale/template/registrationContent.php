d6536b3c2c55746082f3e350b43c3bcabfb30cbb
Francesco <francesco.padovani2@studio.unibo.it>
2022-02-07 17:07:46 +0100

    login error

--- Elaborato_finale/template/registrationContent.php
+++ Elaborato_finale/template/registrationContent.php
@@ -1,6 +1,6 @@
 <section class=" border border-info  bg-opacity-10" name="mainSection">
                 <h2 name="subTitle">Registrati ora: </h2>
-                
+                <p class="text-center text-danger"></p>
                 <form action="registration.php" method="POST">
                     <label id="demo">
                         Inserisci il nuovo username: 
