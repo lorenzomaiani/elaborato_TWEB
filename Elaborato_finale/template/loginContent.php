ae85a6a00014a72cada925c32e9ef60125359e55
LorenzoMaiani0 <73821423+lorenzomaiani@users.noreply.github.com>
2022-02-07 17:07:58 +0100

    Update loginContent.php

--- Elaborato_finale/template/loginContent.php
+++ Elaborato_finale/template/loginContent.php
@@ -1,5 +1,6 @@
 <section class=" border border-info  bg-opacity-10" name="mainSection">
                 <h2 name="subTitle">Inserisci le seguenti informazioni</h2>
+				
                 
                 <form action="index.php" method="POST">
                     <label id="demo">
