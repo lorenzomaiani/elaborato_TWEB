2fb5851a2fda6636390bff91acb04c90d95f3be8
LorenzoMaiani0 <73821423+lorenzomaiani@users.noreply.github.com>
2022-02-09 15:55:45 +0100

    Resolve "required" problem
    Resolve "required" problem

--- Elaborato_finale/template/homepageUser.php
+++ Elaborato_finale/template/homepageUser.php
@@ -22,7 +22,7 @@
                             <p class="card-text"><?php echo $prodotti["descrizioneprodotto"]?></p>
                             <h4><?php echo $prodotti["prezzoprodotto"]?>€</h4>
                             <form action="shop.php" method="POST">
-                                <label>Inserire la quantità: <input type="number" name="quantitàprodottoutente" min="1" max="<?php echo $prodotti["quantitàprodotto"]?>" require></label>
+                                <label>Inserire la quantità: <input type="number" name="quantitàprodottoutente" min="1" max="<?php echo $prodotti["quantitàprodotto"]?>" required></label>
                                 <input type="submit" value="Aggiungi al carrello" role="button" class="btn btn-info" style= "width: 100%; "/>
                                 <input type="hidden" name="action" value="1"/>
                                 <input type="hidden" name="username" value="<?php echo $_SESSION["username"]?>"/>
