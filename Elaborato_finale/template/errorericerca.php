3cd7addfbffa0d2371663ee44ecfbe10f256fa0e
Francesco <francesco.padovani2@studio.unibo.it>
2022-02-09 15:49:53 +0100

    Update errorericerca.php

--- Elaborato_finale/template/errorericerca.php
+++ Elaborato_finale/template/errorericerca.php
@@ -1,5 +1,5 @@
 <section>
-<div class="container bg-gradient p-2 bg-opacity-50  bg-light ">
+<div class="container bg-gradient p-2 bg-opacity-100  bg-light ">
     <div class="row text-center">
     <h2 class="text-danger">Errore nella ricerca del prodotto: <?php echo $params["errorericerca"]?></h2>
   </div>
